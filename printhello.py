6b6bd96beb04fcfa4666c43208f02c6f6d372412
HEMALATHARENGASAMY <35997559+HEMALATHARENGASAMY@users.noreply.github.com>
2018-06-05 15:57:38 +0530

    Update and rename printhello.py to sum.py

--- printhello.py
+++
@@ -1,4 +0,0 @@
-# your code goes
-c=input("enter input:\n")
-for i in range(0,c):
-    print("Hello")
---
+++ sum.py
@@ -0,0 +1,7 @@
+# your code goes
+sum=0
+c=input("enter input:\n")
+a=c+1
+for i in range(0,a):
+    sum=sum+i
+print(sum)
