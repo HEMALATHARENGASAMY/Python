6ec7467de1c3bf92ed1c2cdedf1cedfcf8adb5d7
HEMALATHARENGASAMY <35997559+HEMALATHARENGASAMY@users.noreply.github.com>
2018-06-05 13:02:55 +0530

    Update and rename pos.py to odd.py

---
+++ odd.py
@@ -0,0 +1,7 @@
+# your code goes here
+# your code goes here
+a=int(input("enter the value:"))
+if(a%2==0):
+    print("even")
+else:
+    print("odd")
--- pos.py
+++
@@ -1,8 +0,0 @@
-# your code goes here
-a=int(input("enter the value:"))
-if(a<0):
-	print("negative")
-elif(a>0):
-	print("positive")
-else:
-	print("zero")
