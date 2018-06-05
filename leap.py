454f03934eca2788b2d216226768bc1dc5fdf4dc
HEMALATHARENGASAMY <35997559+HEMALATHARENGASAMY@users.noreply.github.com>
2018-06-05 15:45:05 +0530

    Update and rename leap.py to printhello.py

--- leap.py
+++
@@ -1,6 +0,0 @@
-# your code goes
-ch=input("enter input:")
-if(ch%4==0):
-    print("yes")
-else:
-    print("no")
---
+++ printhello.py
@@ -0,0 +1,4 @@
+# your code goes
+c=input("enter input:\n")
+for i in range(0,c):
+    print("Hello")
