33e59446710db6d78640615c1ba16876fdf47398
HEMALATHARENGASAMY <35997559+HEMALATHARENGASAMY@users.noreply.github.com>
2018-06-05 15:35:22 +0530

    Update and rename alphabet.py to leap.py

--- alphabet.py
+++
@@ -1,6 +0,0 @@
-# your code goes
-ch=raw_input("enter input:")
-if((ch>='a' and ch<='z')or(ch>='A' and ch<='Z')):
-    print("Alphabet")
-else:
-    print("No")
---
+++ leap.py
@@ -0,0 +1,6 @@
+# your code goes
+ch=input("enter input:")
+if(ch%4==0):
+    print("yes")
+else:
+    print("no")
