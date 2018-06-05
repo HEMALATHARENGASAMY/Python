2d94e92834b500a151f47cc55d9c1ebe7b74a38d
HEMALATHARENGASAMY <35997559+HEMALATHARENGASAMY@users.noreply.github.com>
2018-06-05 15:02:46 +0530

    Update and rename odd.py to vowel.py

--- odd.py
+++
@@ -1,7 +0,0 @@
-# your code goes here
-# your code goes here
-a=int(input("enter the value:"))
-if(a%2==0):
-    print("even")
-else:
-    print("odd")
---
+++ vowel.py
@@ -0,0 +1,6 @@
+# your code goes
+ch=raw_input("enter a letter:")
+if(ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U' or ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):   
+    print("vowel")
+else:
+    print("consonant")
